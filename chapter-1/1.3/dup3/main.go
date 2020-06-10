c78f222890431263a6f009a2fb6f06a4ca465cf8
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-10 14:40:17 -0600

    Add dup3 exercise

---
+++ chapter-1/1.3/dup3/main.go
@@ -0,0 +1,29 @@
+// Dup3 prints the count and text of lines that appear more than once in files
+// passed in as arguments.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
+}
