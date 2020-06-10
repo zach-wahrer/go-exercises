4c07267ddc82b27045c1aa3c4be5a73fb54fe7bc
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-10 13:33:04 -0600

    Add echo.go

---
+++ chapter-1/1.2/echo.go
@@ -0,0 +1,16 @@
+// Echo prints the command-line arguments given to it.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var string, seperator string
+	for i := 1; i < len(os.Args); i++ {
+		string += seperator + os.Args[i]
+		seperator = " "
+	}
+	fmt.Println(string)
+}
