17fd8893fa9e82b38bbe041cbc1eb947fa2742d0
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-11 11:10:51 -0600

    Modify 1.4 for exercise 1.6

--- chapter-1/1.4/main.go
+++ chapter-1/1.4/main.go
@@ -28,7 +28,11 @@ import (
 
 //!+main
 
-var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0x00, 0xff, 0x00, 0xff},
+	color.RGBA{0xff, 0x00, 0x00, 0xff},
+	color.RGBA{0x00, 0x00, 0xff, 0xff}}
 
 const (
 	whiteIndex = 0 // first color in palette
@@ -74,7 +78,7 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				uint8(rand.Intn(3)+1))
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
