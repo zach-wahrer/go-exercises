28c86f5d1f9d100aac592fddba62272011f09dec
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-11 10:56:28 -0600

    Modify 1.4 for exercise 1.5

--- chapter-1/1.4/main.go
+++ chapter-1/1.4/main.go
@@ -28,7 +28,7 @@ import (
 
 //!+main
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
 const (
 	whiteIndex = 0 // first color in palette
