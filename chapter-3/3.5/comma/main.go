ca959897853e120340860f8da93e5e81e97a925c
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-14 12:45:46 -0600

    Update comma for exercise 3.11S

--- chapter-3/3.5/comma/main.go
+++ chapter-3/3.5/comma/main.go
@@ -4,21 +4,44 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println(comma("1234567890000"))
+	fmt.Println(comma("-1346711.89812"))
 }
 
 func comma(s string) string {
-	reversed := reverseString(s)
+	whole := s
+	var fraction string
 	var buffer bytes.Buffer
-	for i := len(reversed) - 1; i >= 0; i-- {
-		buffer.WriteByte(byte(reversed[i]))
+
+	if strings.Contains(s, ".") {
+		sides := strings.Split(s, ".")
+		whole, fraction = sides[0], sides[1]
+	}
+
+	if strings.Contains(whole, "-") {
+		whole = whole[1:]
+		buffer.WriteByte('-')
+	}
+
+	reversedWhole := reverseString(whole)
+
+	for i := len(reversedWhole) - 1; i >= 0; i-- {
+		buffer.WriteByte(byte(reversedWhole[i]))
 		if i > 0 && i%3 == 0 {
 			buffer.WriteByte(',')
 		}
 	}
+
+	if fraction != "" {
+		buffer.WriteByte('.')
+		for i := 0; i < len(fraction); i++ {
+			buffer.WriteByte(byte(fraction[i]))
+		}
+	}
+
 	return buffer.String()
 }
 
