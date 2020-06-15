780c6378d28dd5795b36684f90e055beefb95223
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-15 11:26:20 -0600

    Add YB for sizeConst exercise

--- chapter-3/3.6/sizeConst.go
+++ chapter-3/3.6/sizeConst.go
@@ -2,11 +2,12 @@
 package main
 
 const (
-	KB = 1000
-	MB = KB * KB
-	GB = MB * MB
-	TB = GB * GB
-	PB = TB * TB
-	EB = float64(PB * PB)
-	ZB = float64(EB * EB)
+	KB         = 1000
+	MB         = KB * KB
+	GB         = MB * MB
+	TB         = GB * GB
+	PB         = TB * TB
+	EB float64 = PB * PB
+	ZB float64 = EB * EB
+	YB float64 = EB * EB
 )
