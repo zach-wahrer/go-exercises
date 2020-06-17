75f81b7b8f72788eef927235f548084a6cfd6b00
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-17 12:36:42 -0600

    Fix URL const and remove debugging print in search

--- chapter-4/4.5/movieData/search.go
+++ chapter-4/4.5/movieData/search.go
@@ -8,14 +8,13 @@ import (
 	"os"
 )
 
-const OMDBURL = "http://www.omdbapi.com/?apikey=[yourkey]&"
+const OMDBURL = "http://www.omdbapi.com/?apikey="
 
 func buildSearchURL(title string) string {
 	APIKey := os.Getenv("OMDB_API_KEY")
 	if APIKey == "" {
 		log.Fatal("You must set an API key for OMDB - ie.: `export OMDB_API_KEY=your_key_here`")
 	}
-	fmt.Printf("%s%s&s=%s", OMDBURL, APIKey, title)
 	return fmt.Sprintf("%s%s&s=%s", OMDBURL, APIKey, title)
 
 }
