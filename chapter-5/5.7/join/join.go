e66782cb0470460fdef4091986a385823a5b4cfd
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-19 09:07:49 -0600

    Add string joiner implementation

---
+++ chapter-5/5.7/join/join.go
@@ -0,0 +1,33 @@
+// Join is a reimplementation of strings.Join
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(Join(".", "test", "this", "joiner"))
+	fmt.Println(Join("-", "single"))
+	fmt.Println(Join("-"))
+}
+
+func Join(sep string, strings ...string) string {
+	if len(strings) == 0 {
+		return ""
+	}
+	if len(strings) == 1 {
+		return strings[0]
+	}
+
+	var buf []byte
+	buffer := bytes.NewBuffer(buf)
+	for i, value := range strings {
+		buffer.Write([]byte(value))
+		if i != len(strings)-1 {
+			buffer.Write([]byte(sep))
+		}
+	}
+
+	return buffer.String()
+}
