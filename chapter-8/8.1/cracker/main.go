d5067ef558623be0aa48b9ce9a318db6d89ba828
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-23 12:37:24 -0600

    Add basic concurrency for password cracker
    Would be better if it exited once password is found, but haven't learned out how to do that yet.

--- chapter-8/8.1/cracker/main.go
+++ chapter-8/8.1/cracker/main.go
@@ -3,56 +3,59 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
-	"log"
 	"time"
 )
 
-const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*,./1234567890"
+const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
 
 func main() {
 	go waiting(100 * time.Millisecond)
-	testPass := encrypter("A/3@a0")
-	crackedPass, err := cracker(0, len(alphabet), testPass)
-	if err != nil {
-		log.Printf("cracker: %v", err)
-	}
-	if crackedPass == "" {
-		fmt.Println("Password not cracked")
-		return
-	}
-	fmt.Printf("Password is: %s\n", crackedPass)
+	testPass := encrypter("o12zab")
+	go cracker(0, 6, testPass)
+	go cracker(6, 12, testPass)
+	go cracker(12, 18, testPass)
+	go cracker(18, 24, testPass)
+	go cracker(24, 30, testPass)
+	cracker(30, len(alphabet), testPass)
+
 }
 
-func cracker(start, finish int, target [16]byte) (result string, err error) {
+func cracker(start, finish int, target [16]byte) {
 	for _, char1 := range alphabet[start:finish] {
 		pass := string(char1)
 		if encrypter(pass) == target {
-			return pass, nil
+			fmt.Printf("Password is: %s\n", pass)
+			return
 		}
 		for _, char2 := range alphabet {
 			pass = string(char1) + string(char2)
 			if encrypter(pass) == target {
-				return pass, nil
+				fmt.Printf("Password is: %s\n", pass)
+				return
 			}
 			for _, char3 := range alphabet {
 				pass = string(char1) + string(char2) + string(char3)
 				if encrypter(pass) == target {
-					return pass, nil
+					fmt.Printf("Password is: %s\n", pass)
+					return
 				}
 				for _, char4 := range alphabet {
 					pass = string(char1) + string(char2) + string(char3) + string(char4)
 					if encrypter(pass) == target {
-						return pass, nil
+						fmt.Printf("Password is: %s\n", pass)
+						return
 					}
 					for _, char5 := range alphabet {
 						pass = string(char1) + string(char2) + string(char3) + string(char4) + string(char5)
 						if encrypter(pass) == target {
-							return pass, nil
+							fmt.Printf("Password is: %s\n", pass)
+							return
 						}
 						for _, char6 := range alphabet {
 							pass = string(char1) + string(char2) + string(char3) + string(char4) + string(char5) + string(char6)
 							if encrypter(pass) == target {
-								return pass, nil
+								fmt.Printf("Password is: %s\n", pass)
+								return
 							}
 						}
 					}
@@ -61,7 +64,7 @@ func cracker(start, finish int, target [16]byte) (result string, err error) {
 			}
 		}
 	}
-	return "", nil
+	fmt.Printf("Password not found\n")
 }
 
 func encrypter(s string) [16]byte {
