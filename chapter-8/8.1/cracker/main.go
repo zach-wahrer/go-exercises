46b2a47be5466eefc803d281281637052520c23f
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-25 11:02:13 -0600

    Refactor cracker to spawn recursive password checks

--- chapter-8/8.1/cracker/main.go
+++ chapter-8/8.1/cracker/main.go
@@ -10,67 +10,50 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
 
 func main() {
 	go waiting(100 * time.Millisecond)
-	testPass := encrypter("5aaaaa")
+	testPass := encrypter("700a0a")
+	targetLen := 6
 
 	chunkSize := int(len(alphabet) / 6)
-	start := 0
-	end := chunkSize
+	start, end := 0, chunkSize
 	for i := 0; i < chunkSize; i++ {
 		if i == chunkSize-1 {
-			// go cracker(start, len(alphabet), testPass)
+			// go cracker(start, len(alphabet), length testPass)
 		} else {
-			go cracker(start, end, testPass)
+			go cracker(start, end, targetLen, testPass)
 		}
 		start, end = end, end+chunkSize
 	}
-	cracker(30, len(alphabet), testPass)
+	cracker(30, len(alphabet), targetLen, testPass)
 }
 
-func cracker(start, finish int, target [16]byte) {
+func cracker(start, finish, targetLen int, target [16]byte) {
 	for _, char1 := range alphabet[start:finish] {
-		pass := string(char1)
-		if encrypter(pass) == target {
-			fmt.Printf("Password is: %s\n", pass)
-			return
-		}
-		for _, char2 := range alphabet {
-			pass = string(char1) + string(char2)
-			if encrypter(pass) == target {
-				fmt.Printf("Password is: %s\n", pass)
-				return
-			}
-			for _, char3 := range alphabet {
-				pass = string(char1) + string(char2) + string(char3)
-				if encrypter(pass) == target {
-					fmt.Printf("Password is: %s\n", pass)
-					return
-				}
-				for _, char4 := range alphabet {
-					pass = string(char1) + string(char2) + string(char3) + string(char4)
-					if encrypter(pass) == target {
-						fmt.Printf("Password is: %s\n", pass)
-						return
-					}
-					for _, char5 := range alphabet {
-						pass = string(char1) + string(char2) + string(char3) + string(char4) + string(char5)
-						if encrypter(pass) == target {
-							fmt.Printf("Password is: %s\n", pass)
-							return
-						}
-						for _, char6 := range alphabet {
-							pass = string(char1) + string(char2) + string(char3) + string(char4) + string(char5) + string(char6)
-							if encrypter(pass) == target {
-								fmt.Printf("Password is: %s\n", pass)
-								return
-							}
-						}
-					}
-				}
-
-			}
+		if passEqual(string(char1), target) {
+			fmt.Printf("\tPassword found: %s", string(char1))
+		} else {
+			recursiveCrack(string(char1), 2, targetLen, target)
 		}
 	}
-	fmt.Printf("Password not found\n")
+}
+
+func recursiveCrack(chars string, currLen, targetLen int, target [16]byte) {
+	if currLen <= targetLen {
+		for _, char := range alphabet {
+			if passEqual(chars+string(char), target) {
+				fmt.Printf("\tPassword found: %s", chars+string(char))
+				return
+			}
+
+			recursiveCrack(chars+string(char), currLen+1, targetLen, target)
+
+		}
+	}
+	return
+
+}
+
+func passEqual(pass string, target [16]byte) bool {
+	return encrypter(pass) == target
 }
 
 func encrypter(s string) [16]byte {
