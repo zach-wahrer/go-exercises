e362fbe270e8757d68602e235c053e1a87fabfbc
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-26 12:49:36 -0600

    Add select statement and clearer channel struct

--- chapter-8/8.1/cracker/main.go
+++ chapter-8/8.1/cracker/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"fmt"
 	"math"
+	"os"
 	"time"
 )
 
@@ -14,62 +15,66 @@ type target struct {
 	maxLen     int
 }
 
+type comms struct {
+	found    chan string
+	notFound chan struct{}
+}
+
 func main() {
 	go waiting(100 * time.Millisecond)
 
-	targetString := "gh0"
+	targetString := "aslm0"
 	targetPass := target{encrypter(targetString), len(targetString)}
 
-	out := make(chan string)
+	channels := comms{make(chan string), make(chan struct{})}
 
 	chunkSize := int(math.Max(float64(len(alphabet)/6), 1))
 	start, end := 0, chunkSize
 	for i := 0; i < chunkSize; i++ {
 		if i == chunkSize-1 {
-			go crackChunk(start, len(alphabet), targetPass, out)
+			go crackChunk(start, len(alphabet), targetPass, channels)
 		} else {
-			go crackChunk(start, end, targetPass, out)
+			go crackChunk(start, end, targetPass, channels)
 		}
 		start, end = end, end+chunkSize
 	}
 
 	running := chunkSize
-	for reply := range out {
-		if reply == "" {
-			running--
-		} else {
+	for {
+		select {
+		case reply := <-channels.found:
 			fmt.Printf("Password found: %s\n", reply)
-			break
-		}
-
-		if running == 0 {
-			fmt.Println("Password not found.")
-			break
+			os.Exit(0)
+		case <-channels.notFound:
+			running--
+			if running == 0 {
+				fmt.Println("Password not found.")
+				os.Exit(0)
+			}
 		}
 	}
-
 }
 
-func crackChunk(start, finish int, targetPass target, out chan<- string) {
+func crackChunk(start, finish int, targetPass target, channels comms) {
 	for _, char1 := range alphabet[start:finish] {
 		if passEqual(string(char1), targetPass.hashedPass) {
-			out <- string(char1)
+			channels.found <- string(char1)
 			return
 		}
-		recursiveCrack(string(char1), 2, targetPass, out)
+		recursiveCrack(string(char1), 2, targetPass, channels)
 	}
-	out <- ""
+	channels.notFound <- struct{}{}
 	return
 }
 
-func recursiveCrack(chars string, currLen int, targetPass target, out chan<- string) {
+func recursiveCrack(chars string, currLen int, targetPass target, channels comms) {
 	if currLen <= targetPass.maxLen {
 		for _, char := range alphabet {
 			if passEqual(chars+string(char), targetPass.hashedPass) {
-				out <- chars + string(char)
+				channels.found <- chars + string(char)
 				return
 			}
-			recursiveCrack(chars+string(char), currLen+1, targetPass, out)
+			recursiveCrack(chars+string(char), currLen+1, targetPass, channels)
 		}
 	}
 	return
