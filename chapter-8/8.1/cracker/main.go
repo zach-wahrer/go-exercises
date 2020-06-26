a81533c4ac6c52ad6a53203d423c31796a333bf1
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-26 11:15:31 -0600

    Revert "Refactor to use select multiplex for main function" Select syntax didn't work as expected This reverts commit 2d4e23767f67cbfc2cbadb30bf542927bf3da33e.

--- chapter-8/8.1/cracker/main.go
+++ chapter-8/8.1/cracker/main.go
@@ -7,65 +7,65 @@ import (
 	"time"
 )
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890@$."
+const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
 
 func main() {
 	go waiting(100 * time.Millisecond)
 
-	targetString := "@.ax."
+	targetString := "gh0"
 	testPass := encrypter(targetString)
 	targetLen := len(targetString)
 
-	found := make(chan string)
-	notFound := make(chan struct{})
+	out := make(chan string)
 
 	chunkSize := int(math.Max(float64(len(alphabet)/6), 1))
 	start, end := 0, chunkSize
 	for i := 0; i < chunkSize; i++ {
 		if i == chunkSize-1 {
-			go cracker(start, len(alphabet), targetLen, testPass, found, notFound)
+			go cracker(start, len(alphabet), targetLen, testPass, out)
 		} else {
-			go cracker(start, end, targetLen, testPass, found, notFound)
+			go cracker(start, end, targetLen, testPass, out)
 		}
 		start, end = end, end+chunkSize
 	}
 
 	running := chunkSize
-	select {
-	case reply := <-found:
-		fmt.Printf("Password found: %s\n", reply)
-		break
-	case <-notFound:
-		running--
+	for reply := range out {
+		if reply == "" {
+			running--
+		} else {
+			fmt.Printf("Password found: %s\n", reply)
+			break
+		}
+
 		if running == 0 {
 			fmt.Println("Password not found.")
 			break
 		}
-
 	}
 
 }
 
-func cracker(start, finish, targetLen int, target [16]byte, found chan<- string, notFound chan<- struct{}) {
+func cracker(start, finish, targetLen int, target [16]byte, out chan<- string) {
 	for _, char1 := range alphabet[start:finish] {
 		if passEqual(string(char1), target) {
-			found <- string(char1)
+			out <- string(char1)
 			return
 		}
-		recursiveCrack(string(char1), 2, targetLen, target, found)
+		recursiveCrack(string(char1), 2, targetLen, target, out)
 	}
-	notFound <- struct{}{}
+	out <- ""
 	return
 }
 
-func recursiveCrack(chars string, currLen, targetLen int, target [16]byte, found chan<- string) {
+func recursiveCrack(chars string, currLen, targetLen int, target [16]byte, out chan<- string) {
 	if currLen <= targetLen {
 		for _, char := range alphabet {
 			if passEqual(chars+string(char), target) {
-				found <- chars + string(char)
+				out <- chars + string(char)
 				return
 			}
-			recursiveCrack(chars+string(char), currLen+1, targetLen, target, found)
+			recursiveCrack(chars+string(char), currLen+1, targetLen, target, out)
 		}
 	}
 	return
